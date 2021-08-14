483a693e8f1b823d0ddb28ffb954cf1155a65d9f
Xi LIU <e0385051@u.nus.edu>
2021-08-14 20:41:39 +0800

    one line deleted

--- wkbp4/plotAllFuncs4R11.py
+++ wkbp4/plotAllFuncs4R11.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 # this script combines calculations from WKB
 # and shooting as a verification for potential 4, V(x)=x^{2}-igx^{5}
-
+# in region I-I
 # WKB part
 def chooseX1(xTmp):
     '''
@@ -95,7 +95,8 @@ def simpInt(g, E, N, x1, x2):
                                                 + f(zAll[N], g, E)
                                                 )
 
-def testRoots(g,E):
+
+def testRoots(g, E):
     '''
 
     :param g: const
@@ -104,47 +105,48 @@ def testRoots(g,E):
     '''
     coefs = [-1j * g, 0, 0, 1, 0, -E]
     rootsAll = np.roots(coefs)
-    test1=False
-    test2=False
+    test1 = False
+    test2 = False
     for rtTmp in rootsAll:
-        test1=test1 or chooseX1(rtTmp)
+        test1 = test1 or chooseX1(rtTmp)
     for rtTmp in rootsAll:
-        test2=test2 or chooseX2(rtTmp)
+        test2 = test2 or chooseX2(rtTmp)
     return (test1 and test2)
 
 
-
-def eqn(EIn,*data):
+def eqn(EIn, *data):
     '''
 
     :param EIn: trial eigenvalue, in the form of [re, im]
     :return:
     '''
 
-    n,g=data
+    n, g = data
 
     E = EIn[0] + 1j * EIn[1]
-    x1, x2 = retX1X2(g,E)
+    x1, x2 = retX1X2(g, E)
     dx = 1e-4
     N = int(np.abs(x2 - x1) / dx)
-    rst = simpInt(g,E, N, x1, x2) - (n+1/2) * np.pi
+    rst = simpInt(g, E, N, x1, x2) - (n + 1 / 2) * np.pi
     return np.real(rst), np.imag(rst)
+
 
 ###functions for WKB end here
 
-#shooting part
+# shooting part
 # L = 10
 # Ns = 1000
 # h = -np.sign(L) * L / Ns
 
-def Q(x, g,E):
+def Q(x, g, E):
     return x ** 2 - 1j * g * x ** 5 - E
 
 
-def f1(x,g, E):
-    return -1 / 4 * (2 * x - 5 * 1j * g * x ** 4) / Q(x,g, E) -Q(x,g, E) ** (1 / 2)
+def f1(x, g, E):
+    return -1 / 4 * (2 * x - 5 * 1j * g * x ** 4) / Q(x, g, E) - Q(x, g, E) ** (1 / 2)
 
-def oneStepRk4(g,E, h,xCurr, yCurr, vCurr):
+
+def oneStepRk4(g, E, h, xCurr, yCurr, vCurr):
     '''
 
     :param E:
@@ -153,51 +155,51 @@ def oneStepRk4(g,E, h,xCurr, yCurr, vCurr):
     :param vCurr:
     :return: yNext, vNext
     '''
-    M0 = h * Q(xCurr, g,E) * yCurr
+    M0 = h * Q(xCurr, g, E) * yCurr
 
-    M1 = h * Q(xCurr + 1 / 2 * h, g,E) * (yCurr + 1 / 2 * h * vCurr)
+    M1 = h * Q(xCurr + 1 / 2 * h, g, E) * (yCurr + 1 / 2 * h * vCurr)
 
-    M2 = h * Q(xCurr + 1 / 2 * h,g, E) * (yCurr + 1 / 2 * h * vCurr + 1 / 4 * h * M0)
+    M2 = h * Q(xCurr + 1 / 2 * h, g, E) * (yCurr + 1 / 2 * h * vCurr + 1 / 4 * h * M0)
 
-    M3 = h * Q(xCurr + h, g,E) * (yCurr + h * vCurr + 1 / 2 * h * M1)
+    M3 = h * Q(xCurr + h, g, E) * (yCurr + h * vCurr + 1 / 2 * h * M1)
 
     yNext = yCurr + h * vCurr + 1 / 6 * h * (M0 + M1 + M2)
     vNext = vCurr + 1 / 6 * (M0 + 2 * M1 + 2 * M2 + M3)
 
     return yNext, vNext
 
-def calculateBoundaryValue(E,*data):
+
+def calculateBoundaryValue(E, *data):
     # L = 10
     # Ns = 1000
     g = data[0]
-    E=E[0]
-    coefs = [-1j * g, 0, 0, 1, 0, -E]
+    E = E[0]
+
 
     # rootsAll = np.roots(coefs)
 
     # print(rootsAll)
     # rootsAll = sorted(rootsAll, key=np.angle, reverse=True)
     # x1Val = rootsAll[2]
-    L=10#4*np.abs(np.real(x1Val))
+    L = 10  # 4*np.abs(np.real(x1Val))
     # print(L)
-    theta=1/14*np.pi
-    hAbsEst=1e-2
-    Ns=int(L/hAbsEst)
-    h = -L/Ns*np.exp(1j*(theta))
-    x0=L*np.exp(1j*theta)
+    theta = 1 / 14 * np.pi
+    hAbsEst = 1e-2
+    Ns = int(L / hAbsEst)
+    h = -L / Ns * np.exp(1j * (theta))
+    x0 = L * np.exp(1j * theta)
     xAll = [x0]
     yAll = [1]
 
-    vAll = [f1(x0,g, E)]
+    vAll = [f1(x0, g, E)]
     for j in range(0, Ns):
         xCurr = xAll[-1]
         yCurr = yAll[-1]
         vCurr = vAll[-1]
-        yNext, vNext = oneStepRk4(g,E,h, xCurr, yCurr, vCurr)
+        yNext, vNext = oneStepRk4(g, E, h, xCurr, yCurr, vCurr)
         xNext = xCurr + h
         xAll.append(xNext)
         yAll.append(yNext)
         vAll.append(vNext)
     # boundary condition: Re(vN/yN)=0
     return np.real(vAll[-1] / yAll[-1])
-
