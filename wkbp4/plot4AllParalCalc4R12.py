d86bf87da1919d9cc863d2264d902db5e2f30895
Xi LIU <e0385051@u.nus.edu>
2021-08-18 23:07:50 +0800

    added approximation

--- wkbp4/plot4AllParalCalc4R12.py
+++ wkbp4/plot4AllParalCalc4R12.py
@@ -3,23 +3,38 @@ from plot4AllFuncs4R12 import *
 # this script calculates and plots eigenvalues, potential 4, V(x)=x^{2}-igx^{5}
 # Region I-II
 
-gnIndAll = np.linspace(start=np.log10(0.05), stop=np.log10(0.2), num=1)
+gnIndAll = np.linspace(start=np.log10(1e-3), stop=np.log10(1e-1), num=20)
 gAll = [10 ** elem for elem in gnIndAll]
 EWKB = []
+
+
+
+y2=np.exp(-1j*5/6*np.pi)
+y1=np.exp(-1j*1/6*np.pi)
 # calculate WKB eigenvalues
 
 # calculate WKB eigenvalues
 threadNum = 12
 energyLevelMax = 30
 levelsAll = range(0, energyLevelMax + 1)
-inDataAll = []
+inDataAll=[]
 for nTmp in levelsAll:
     for gTmp in gAll:
-        inDataAll.append((nTmp, gTmp))
+        EEst=2*((nTmp+1/2)*np.pi-gTmp**(-2/3)*I4(y2,y1))/I5(y2,y1)
+        inDataAll.append([nTmp,gTmp,EEst])
+        inDataAll.append([nTmp,gTmp,np.conj(EEst)])
 
 tWKBParalStart = datetime.now()
 pool1 = Pool(threadNum)
-retAll = pool1.map(computeOneSolution, inDataAll)
+retAll=pool1.map(computeOneSolutionWithInit,inDataAll)
+# inDataAll = []
+# for nTmp in levelsAll:
+#     for gTmp in gAll:
+#         inDataAll.append((nTmp, gTmp))
+#
+# tWKBParalStart = datetime.now()
+# pool1 = Pool(threadNum)
+# retAll = pool1.map(computeOneSolution, inDataAll)
 
 tWKBParalEnd = datetime.now()
 print("WKB time: ", tWKBParalEnd - tWKBParalStart)
@@ -27,7 +42,7 @@ tPltStart = datetime.now()
 # plot WKB
 fig, ax = plt.subplots(figsize=(20, 20))
 
-ax.set_xscale('log')
+# ax.set_xscale('log')
 ax.set_yscale('symlog')
 ax.set_ylabel("E")
 ax.set_xlabel("g")
@@ -46,10 +61,27 @@ for itemTmp in retAll:
     ERealSctVals.append(ERe)
     EImagSctVals.append(EIm)
 
+partitionByN=dict()
+energyLevelSet=set(nSctVals)
+for nTmp in energyLevelSet:
+    partitionByN[nTmp]=[]
+
+#data partition
+for itemTmp in retAll:
+    nTmp,gTmp,ERe,EIm=itemTmp
+    partitionByN[nTmp].append([gTmp,ERe,EIm])
+
+#plot for the same energy level
+# for nTmp in partitionByN.keys():
+#     gVecTmp=[]
+#     EReVecTmp=[]
+#     for vecTmp in partitionByN[nTmp]:
+#         gVecTmp.append(vecTmp[0])
+#         EReVecTmp.append(vecTmp[1])
+#     ax.plot(gVecTmp,EReVecTmp,color="red")
 sctRealPartWKB = ax.scatter(gSctVals, ERealSctVals, color="red", marker=".", s=50, label="WKB real part")
 plt.legend()
-plt.savefig("tmp12.png")
+plt.savefig("tmp121.png")
 
 tPltEnd = datetime.now()
 print("plotting time: ", tPltEnd - tPltStart)
-print(retAll)
